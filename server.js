121e1706210f40ce4201fa6095430818ee75d903
teresarod11 <rodriguez49372@gmail.com>
2023-02-10 14:03:04 -0500

    header added

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const routes = require("./controllers/index.js");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
+app.engine("handlebars", hbs.engine);
 
+app.set("view engine", "handlebars");
 // Makes express use the routes
+app.use(express.static('layouts'))
 app.use(routes);
-
 
 app.listen(PORT, () => {
     console.log("It is working")
