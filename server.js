55f3c4bd819a7a3abbf8ec95eea9deedbe2be6cc
Mike <nicholasfierke97@gmail.com>
2023-02-10 10:56:55 -0500

    Server.js and Index.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const routes = require("./controllers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Makes express use the routes
+app.use(routes);
+
 
 app.listen(PORT, () => {
     console.log("It is working")
