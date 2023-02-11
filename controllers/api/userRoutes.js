753aaad9388ad6fa613ab3470c8cdd6875937c6d
Mike <nicholasfierke97@gmail.com>
2023-02-11 15:35:18 -0500

    Route folder template?

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// Login page route
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Logout page route
+router.get('/logout', (req, res) => {
+  // Implement logout logic, for example by destroying the session
+  req.session.destroy((err) => {
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
