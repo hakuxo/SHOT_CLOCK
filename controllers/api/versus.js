ce7bddd5577bf6f3e6939046fe35717cbcf86c8c
Nya Tyus <nyastyus@gmail.com>
2023-02-13 13:16:40 -0500

    fix cookie issue in chrome

--- controllers/api/versus.js
+++ controllers/api/versus.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+  res.render("schedule");
+});
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,13 +5,25 @@ const models = require("./models");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({});
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+    sameSite: true,
+  },
+  resave: false,
+  saveUninitialized: true,
+};
+
 app.engine("handlebars", hbs.engine);
 
 app.set("view engine", "handlebars");
 // Makes express use the routes
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(routes);
 
 app.listen(PORT, () => {
-    console.log("It is working")
+  console.log("It is working");
 });
