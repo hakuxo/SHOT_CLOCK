7f1f57ee9e02035ed7f431e9ceb38b9a8e778b92
haku <jmhallmark123@gmail.com>
2023-02-14 14:05:30 -0500

    sally is great at helping

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 let sequelize;
-
+// JAWSDB is helping figure out the error
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
