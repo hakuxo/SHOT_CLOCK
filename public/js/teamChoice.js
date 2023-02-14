d0d3159a7514b8081e91dcd184f8c08ba35f1bd3
Nya Tyus <nyastyus@gmail.com>
2023-02-13 21:22:09 -0500

    add header to schedule handlebars

--- public/js/teamChoice.js
+++ public/js/teamChoice.js
@@ -11,36 +11,20 @@
 //   .then((response) => console.log(response))
 //   .catch((err) => console.error(err));
 
-console.log("hello");
-
-// const team = (event) => {
-//   let clicked = event.target.id;
-//   clicked = document.getAttribute("id");
-//   if (clicked === "hawks") {
-//     console.log("hawks game!");
-//   }
-// };
-
 const choice = document.getElementsByClassName("nba");
 
 const team = (event) => {
   let clicked = event.target;
   console.dir(event.target);
-  // id = clicked.getAttribute("alt");
-  let id = clicked.parentElement.id
-  console.log(id)
-  // if (id === "Atlanta Hawks") {
-  //   console.log("hawks game!");
-  // }
-  window.location.href = ("/api/game/" + id)
+  let id = clicked.parentElement.id;
+  console.log(id);
+  window.location.href = "/api/game/" + id;
 };
+
 for (let i = 0; i < choice.length; i++) {
   const img = choice[i];
-  img.addEventListener("click", team);  
+  img.addEventListener("click", team);
 }
-  
-
-
 
 // brainstorm...
 // function?
