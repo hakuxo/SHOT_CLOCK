92fa123aa351f3d35bf53fc4fdfcbbc0617bf26a
teresarod11 <rodriguez49372@gmail.com>
2023-02-15 01:38:58 -0500

    uncommented logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,14 @@
-// const logoutFormHandler = async (event) => {
-//     event.preventDefault();
+const logoutFormHandler = async (event) => {
+    event.preventDefault();
   
-//     // Remove the user information from local storage
-//     localStorage.removeItem('user');
+    // Remove the user information from local storage
+    localStorage.removeItem('user');
   
-//     // Redirect the user to the login page
-//     document.location.replace('/login');
-//   };
+    // Redirect the user to the login page
+    document.location.replace('/login');
+  };
   
-//   document
-//       .querySelector('.formContainer')
-//       .addEventListener('submit', logoutFormHandler);
+  document
+      .querySelector('.formContainer')
+      .addEventListener('submit', logoutFormHandler);
   
